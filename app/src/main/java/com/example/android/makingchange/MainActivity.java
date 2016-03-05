f06b7493df2cee2c6d32b5a4d615d7221a3f81fe
jdial1 <justinalandial@gmail.com>
2016-03-04 21:40:55 -0600

    Added signin and signup xml files. Started work on Signin and security java files

--- app/src/main/java/com/example/android/makingchange/MainActivity.java
+++ app/src/main/java/com/example/android/makingchange/MainActivity.java
@@ -4,10 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,97 +12,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-
     }
 
-    public void MenuMove(View v) {
-
-        //MAIN MENU BUTTONS
-        final Button MainMenu_BtnSignIn = (Button) findViewById(R.id.mainmenu_button_signin);
-        final Button MainMenu_BtnSignUp = (Button) findViewById(R.id.mainmenu_button_signup);
-        final Button MainMenu_BtnBack = (Button) findViewById(R.id.mainmenu_button_back);
-        final Button MainMenu_BtnOK = (Button) findViewById(R.id.mainmenu_button_ok);
-        final ImageView MainMenu_LogoImage = (ImageView) findViewById(R.id.mainmenu_LogoImageView);
-        //SIGN IN BUTTONS
-        final TextView SignIn_UserNameView = (TextView) findViewById(R.id.signin_txtusername);
-        final EditText SignIn_UserNameField = (EditText) findViewById(R.id.signin_username);
-        final TextView SignIn_PasswordView = (TextView) findViewById(R.id.signin_txtpassword);
-        final EditText SignIn_PasswordField = (EditText) findViewById(R.id.signin_password);
-        //SIGN UP BUTTONS
-        final TextView SignUp_UserNameView = (TextView) findViewById(R.id.signup_txtusername);
-        final EditText SignUp_UserNameField = (EditText) findViewById(R.id.signup_username);
-        final TextView SignUp_PasswordView = (TextView) findViewById(R.id.signup_txtpassword);
-        final EditText SignUp_PasswordField = (EditText) findViewById(R.id.signup_password);
-        final TextView SignUp_PasswordView2 = (TextView) findViewById(R.id.signup_txtpassword2);
-        final EditText SignUp_PasswordField2 = (EditText) findViewById(R.id.signup_password2);
-        final TextView SignUp_EmailView = (TextView) findViewById(R.id.signup_txtemail);
-        final EditText SignUp_EmailField = (EditText) findViewById(R.id.signup_email);
-
+    public void MenuMove(View v) throws Exception {
         switch (v.getId()) {
+
             case R.id.mainmenu_button_back:
-                MainMenu_BtnSignIn.setVisibility(View.VISIBLE);
-                MainMenu_BtnSignUp.setVisibility(View.VISIBLE);
-                MainMenu_LogoImage.setVisibility(View.VISIBLE);
-                SignIn_UserNameField.setVisibility(View.GONE);
-                SignIn_UserNameView.setVisibility(View.GONE);
-                SignIn_PasswordField.setVisibility(View.GONE);
-                SignIn_PasswordView.setVisibility(View.GONE);
-                SignUp_UserNameField.setVisibility(View.GONE);
-                SignUp_UserNameView.setVisibility(View.GONE);
-                SignUp_PasswordField.setVisibility(View.GONE);
-                SignUp_PasswordView.setVisibility(View.GONE);
-                SignUp_PasswordField2.setVisibility(View.GONE);
-                SignUp_PasswordView2.setVisibility(View.GONE);
-                SignUp_EmailField.setVisibility(View.GONE);
-                SignUp_EmailView.setVisibility(View.GONE);
-                MainMenu_BtnBack.setVisibility(View.GONE);
-                MainMenu_BtnOK.setVisibility(View.GONE);
+                setContentView(R.layout.activity_main);
                 break;
 
             case R.id.mainmenu_button_ok:
-                MainMenu_BtnSignIn.setVisibility(View.VISIBLE);
-                MainMenu_BtnSignUp.setVisibility(View.VISIBLE);
-                MainMenu_LogoImage.setVisibility(View.VISIBLE);
-                SignIn_UserNameField.setVisibility(View.GONE);
-                SignIn_UserNameView.setVisibility(View.GONE);
-                SignIn_PasswordField.setVisibility(View.GONE);
-                SignIn_PasswordView.setVisibility(View.GONE);
-                SignUp_UserNameField.setVisibility(View.GONE);
-                SignUp_UserNameView.setVisibility(View.GONE);
-                SignUp_PasswordField.setVisibility(View.GONE);
-                SignUp_PasswordView.setVisibility(View.GONE);
-                SignUp_PasswordField2.setVisibility(View.GONE);
-                SignUp_PasswordView2.setVisibility(View.GONE);
-                SignUp_EmailField.setVisibility(View.GONE);
-                SignUp_EmailView.setVisibility(View.GONE);
-                MainMenu_BtnBack.setVisibility(View.GONE);
-                MainMenu_BtnOK.setVisibility(View.GONE);
+                setContentView(R.layout.activity_main);
+                //SignIn.signin(SignIn_UserNameField.getText().toString(),SignIn_PasswordField.getText().toString());
                 break;
 
             case R.id.mainmenu_button_signin:
-                SignIn_UserNameField.setVisibility(View.VISIBLE);
-                SignIn_UserNameView.setVisibility(View.VISIBLE);
-                SignIn_PasswordField.setVisibility(View.VISIBLE);
-                SignIn_PasswordView.setVisibility(View.VISIBLE);
-                MainMenu_BtnSignIn.setVisibility(View.GONE);
-                MainMenu_BtnSignUp.setVisibility(View.GONE);
-                MainMenu_BtnBack.setVisibility(View.VISIBLE);
-                MainMenu_BtnOK.setVisibility(View.VISIBLE);
-                MainMenu_LogoImage.setVisibility(View.VISIBLE);
+                setContentView(R.layout.signin);
                 break;
 
             case R.id.mainmenu_button_signup:
-                MainMenu_BtnBack.setVisibility(View.VISIBLE);
-                MainMenu_BtnOK.setVisibility(View.VISIBLE);
-                MainMenu_LogoImage.setVisibility(View.GONE);
-                SignUp_UserNameField.setVisibility(View.VISIBLE);
-                SignUp_UserNameView.setVisibility(View.VISIBLE);
-                SignUp_PasswordField.setVisibility(View.VISIBLE);
-                SignUp_PasswordView.setVisibility(View.VISIBLE);
-                SignUp_PasswordField2.setVisibility(View.VISIBLE);
-                SignUp_PasswordView2.setVisibility(View.VISIBLE);
-                SignUp_EmailField.setVisibility(View.VISIBLE);
-                SignUp_EmailView.setVisibility(View.VISIBLE);
+                setContentView(R.layout.signup);
                 break;
         }
 
---
+++ app/src/main/java/com/example/android/makingchange/SignIn.java
@@ -0,0 +1,88 @@
+package com.example.android.makingchange;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class SignIn {
+
+    static final String DB_URL = "jdbc:mysql://104.197.99.130:1433/makingchagedb";
+
+    public static void signin(String USER, String PASS) {
+
+
+        //TESTING HASH PASSWORD
+        String HASHPASS = Security.hashPassword(PASS);
+
+        System.out.println("Username :" + USER);
+        System.out.println("Password :" + PASS);
+        System.out.println("Password Hash :" + HASHPASS);
+
+
+        Connection conn = null;
+        Statement stmt = null;
+
+
+/*        try{
+
+            //STEP 2: Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Cannot find the driver in the classpath!", e);
+        }
+
+        try{
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            System.out.println("The URL is: " + DB_URL+USER+PASS);
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+            //STEP 4: Execute a query
+            System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT FirstName,LastName,Password,Email FROM UserAccounts Where FirstName='Justin'";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            //STEP 5: Extract data from result set
+            while(rs.next()){
+                //Retrieve by column name
+                String firstname  = rs.getString("FirstName");
+                String lastname = rs.getString("LastName");
+                String password = rs.getString("Password");
+                String email = rs.getString("Email");
+
+                //Display values
+                System.out.print("First Name: " + firstname);
+                System.out.print(", Last Name: " + lastname);
+                System.out.print(", Password: " + password);
+                System.out.println(", Email: " + email);
+            }
+            //STEP 6: Clean-up environment
+            rs.close();
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally{
+            //finally block used to close resources
+            try{
+                if(stmt!=null)
+                    stmt.close();
+            }catch(SQLException se2){
+            }// nothing we can do
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+        System.out.println("Goodbye!");
+    }//end main*/
+    }
+}//end FirstExample
